bed7af5f4b32a6594c8ab1b3e2599478c6783012
DauletOmarbek <daulet.omarbek@mail.ru>
2021-04-12 21:51:02 +0600

    1

--- Python/123/3.py
+++
@@ -1,9 +0,0 @@
-a=input().split()
-if len(a[1])==2 and len(a[2])==2:
-    b=int(a[0])*372+int(a[1])*31+int(a[2])
-    if b>1299*372 and b<1923*372:
-        print("YES")
-    else:
-        print("NO")
-else:
-    print("NO") 
