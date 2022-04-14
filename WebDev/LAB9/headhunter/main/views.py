9411da66fc59fedfd64e3038a219fb6da7fc4d1b
Daulet <daulet.omarbek@mail.ru>
2022-04-15 01:40:40 +0600

    .

--- WebDev/LAB9/headhunter/main/views.py
+++ WebDev/LAB9/headhunter/main/views.py
@@ -1,26 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Company, Vacancy
+import json
 
 
 def company_list(request):
-    company = Company.objects.all()
-    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'company': company})
+    companys = Company.objects.all()
+    company_json = [company.to_json() for company in companys]
+    return JsonResponse(company_json, safe=False)
 
 
 def company_details(request, id):
-    company = Company.objects.all()
-    for x in company:
-        if x.id == id + 1:
-            return render(request, 'company_details/index.html', {'title': 'Главная страница', 'x': x})
-    return HttpResponse('<h2>Not found</h2>')
+    try:
+        company = Company.objects.get(id=id)
+    except Company.DoesNotExist as e:
+        return JsonResponse({'message': str(e)}, status=400)
+
+    return JsonResponse(company.to_json())
 
 
 def company_vacancies(request, id):
-    company = Company.objects.all()
-    vacancies = Vacancy.objects.all()
-    for x in company:
-        if x.id == id + 1:
-            return render(request, 'company_vacancies/index.html',
-                          {'title': 'Vacancies', 'y': vacancies, 'name_company': x.name})
-    return HttpResponse('<h2>Not found</h2>')
+    try:
+        company = Company.objects.get(id=id)
+        result = []
+        vacancys = Vacancy.objects.all()
+        vacancy_json = [vacancy.to_json() for vacancy in vacancys]
+        for x in vacancy_json:
+            if x.company == company.name:
+                result.append(x)
+    except Company.DoesNotExist as e:
+        return JsonResponse({'message': str(e)}, status=400)
+
+    return JsonResponse(result, safe=False)
